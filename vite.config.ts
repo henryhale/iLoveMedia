09414bb1840e8f8ed32176d271db910289d699b1
Henry Hale <92443116+henryhale@users.noreply.github.com>
2025-12-30 00:34:12 +0300

    fix(ci): added worker format to esm

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,52 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import vueDevTools from "vite-plugin-vue-devtools"
+import tailwindcss from "@tailwindcss/vite"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/iLoveMedia/',
-  build: {
-    outDir: "./dist/",
-    emptyOutDir: true
-  },
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg,ttf,json}"],
-        maximumFileSizeToCacheInBytes: 40 * 1024 * 1024, // 40MB
-      },
-      injectRegister: 'auto',
-      devOptions: {
-        enabled: process.env.MODE == 'development'
-      }
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./source', import.meta.url))
-    },
-  },
-  server: {
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-    },
-  },
-  preview: {
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-    },
-  },
+	base: "/iLoveMedia/",
+	build: {
+		outDir: "./dist/",
+		emptyOutDir: true,
+	},
+	plugins: [
+		vue(),
+		vueDevTools(),
+		tailwindcss(),
+		VitePWA({
+			registerType: "autoUpdate",
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,ico,png,svg,ttf,json}"],
+				maximumFileSizeToCacheInBytes: 40 * 1024 * 1024, // 40MB
+			},
+			injectRegister: "auto",
+			devOptions: {
+				enabled: process.env.MODE == "development",
+			},
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./source", import.meta.url)),
+		},
+	},
+	server: {
+		headers: {
+			"Cross-Origin-Opener-Policy": "same-origin",
+			"Cross-Origin-Embedder-Policy": "require-corp",
+		},
+	},
+	preview: {
+		headers: {
+			"Cross-Origin-Opener-Policy": "same-origin",
+			"Cross-Origin-Embedder-Policy": "require-corp",
+		},
+	},
+	worker: {
+		format: "es",
+	},
 })
